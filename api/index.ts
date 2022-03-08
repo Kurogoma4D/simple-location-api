0f2e8fd792830c008c85fb0bbad89e89851b57ec
Kurogoma4D <highdro0479@gmail.com>
2022-03-08 17:27:40 +0900

    add: return code

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,4 @@
 import { ServerRequest } from 'https://deno.land/std@0.105.0/http/server.ts';
 
 export default (req: ServerRequest) =>
-  req.respond({ body: JSON.stringify({ success: true }) });
+  req.respond({ body: JSON.stringify({ success: true }) + '\n' });
